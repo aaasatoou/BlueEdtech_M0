04480cd186cf6ee4cf15988800acb615a301fbe0
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2022-02-22 16:57:31 -0300

    Create 2.py

---
+++ Fácil/2.py
@@ -0,0 +1,5 @@
+#Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda e o CEP e telefone na terceira.
+
+print("Victor Mitsuo Asato")
+print("Rua Não vou colocar meu endereço - CEP 0000-000")
+print("(11) 95555-2222")
