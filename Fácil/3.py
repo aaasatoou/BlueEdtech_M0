0000cb739a007ac1575efbfc5d5675f74e6570af
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2022-02-22 17:01:28 -0300

    Create 3.py

---
+++ Fácil/3.py
@@ -0,0 +1,3 @@
+#Faça um programa que mostre na tela uma letra de música que você gosta.
+
+print("By morning it had blown over\nWe ventured out to see what remained\nIt had been creeping closer and closer\nBut so far we'd dodged it time and time again\nI used to live such a simple life, an easy lie\nI was afraid to own up to the truth\nWe're not like you, we will never be content\nWith what we're given\nWe will pursue this to the end\nLeft no trail of crumbs behind\nThe friends that I've had\nHave since become strangers\nAfter the storm it was perfectly clear\nThe ones that I love warned me of dangers\nBut by now there's no place for fear")
