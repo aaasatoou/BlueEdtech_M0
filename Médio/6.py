e11dfd3f940e1dc5d39569e75193f4802a1ffbf5
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2022-02-22 19:31:58 -0300

    Create 6.py

---
+++ Médio/6.py
@@ -0,0 +1,25 @@
+"""Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já esteja lá dentro, ele não será adicionado. 
+No final, serão exibidos todos os valores únicos digitados, em ordem crescente."""
+
+numeros = []
+print("Digite os numeros 1 por vez, digite 'c' para concluir a inserção de valores numéricos")
+
+while True:
+  n = input().strip().lower()
+  if n == 'c':
+    break
+
+  elif n.isnumeric():
+    n = float(n)
+
+    if numeros.count(n)>0:
+      continue
+    else:
+      numeros.append(n)
+
+  else:
+    print("Entrada inválida!!!!")
+  print(numeros)
+
+numeros.sort()
+print(f"Os numeros digitados foram: {numeros}")
