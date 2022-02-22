e6023f985621bfab44af1aebf6b0cd11692acd6e
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2022-02-22 19:48:38 -0300

    Create 1.py

---
+++ Dificil/1.py
@@ -0,0 +1,5 @@
+#Faça um programa, que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno:
+
+larg = float(input("Qual a largura do terreno?\n"))
+comp = float(input("Qual o comprimento do terreno?\n"))
+print(f"Seu terreno tem {larg*comp} metros quadrados")
