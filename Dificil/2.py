4af331b1a35e4b69fc2e4a83ebf082e13c7651fc
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2022-02-22 19:58:39 -0300

    Create 2.py

---
+++ Dificil/2.py
@@ -0,0 +1,9 @@
+#Faça um programa que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições:
+
+ano = int(input("Qual seu ano de nascimento?\n"))
+if 2022 - ano < 16:
+  print("Voto negado!")
+elif 2022 - ano < 18 or 2022-ano >= 70:
+  print("Voto opcional!")
+else:
+  print ("Voto obrigatório")
