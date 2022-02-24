bffac0f4e90d69c5e3206d9f10e4c6dd2b979ca9
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2022-02-23 22:32:02 -0300

    Update 6.py

--- Dificil/6.py
+++ Dificil/6.py
@@ -16,36 +16,36 @@ dic = {"Jogadores":[],
 rodadas = int(input("Quantas rodadas irão ser jogadas? \n"))
 numJ = int(input("Quantos jogadores irão jogar? \n"))
 
-for n in range(0,numJ):
+for n in range(0,numJ): # Preenche os vetores de jogador e placar, funciona como um setup.
   dic["Jogadores"].append(input(f"Nome do jogador na posição {n+1}: ").strip().capitalize())
   dic["Placar"].append(0)
 
-for i in range (0,rodadas):
+for i in range (0,rodadas): # Cria, preenche e resolve as rodadas.
   dic[f"Rodada {i+1}"] = []
-  for n in range (0,numJ):
+  for n in range (0,numJ): # Preenche os valores sorteados nos vetores da respectiva rodada.
     dic[f"Rodada {i+1}"].append(random.randint(1,6))
 
-  print(f"\n{dic[f'Rodada {i+1}']}")
+  print(f"\n{dic[f'Rodada {i+1}']}") # Mostra os valores sorteados de cada jogador para o usuario.
   
-  if dic[f"Rodada {i+1}"].count(max(dic[f"Rodada {i+1}"])) > 1:
+  if dic[f"Rodada {i+1}"].count(max(dic[f"Rodada {i+1}"])) > 1: # Confere quantas correspondencias há do valor máximo dentro do vetor, sendo maior que 1 há um empate.
     print(f"Rodada {i+1} desconsiderada devido ao empate!")
-  else:
-    print(f"Rodada {i+1} é válida!")
+  else:                                                         # Caso não tenha empate, adiciona 1 ao placar do jogador vencedor.
+    print(f"Rodada {i+1} é valida!")
     dic["Placar"][dic[f"Rodada {i+1}"].index(max(dic[f"Rodada {i+1}"]))] += 1
 
 
 
-print(f"\n\nO placar final é: {dic['Placar']}")
+print(f"\n\nO placar final é: {dic['Placar']}") # Exibe o placar final do jogo.
 
-if dic["Placar"].count(max(dic["Placar"])) > 1:
+if dic["Placar"].count(max(dic["Placar"])) > 1: # Verifica quantas correspondeicar há do valor maximo dentro do vetor, maior que 1 o resultado final foi um empate.
 
   print("Empate!")
-  vencedores = []
 
-  for x in range(len(dic["Placar"])):
+  vencedores = []
+  for x in range(len(dic["Placar"])): # No caso de empate, busca o indice dos jogadores empatados na primeira posição para exibir seus nomes.
     if dic["Placar"][x] == max(dic["Placar"]):
       vencedores.append(x)
       print(f"Parabéns {dic['Jogadores'][x]}")
 
 else:
-  print(f"Parabéns {dic['Jogadores'][dic['Placar'].index(max(dic['Placar']))]}, você venceu!")
+  print(f"Parabéns {dic['Jogadores'][dic['Placar'].index(max(dic['Placar']))]}, você venceu!") # Caso não tenha empate, exibe o nome do jogador que possui mais pontos. 
